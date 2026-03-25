cd45b325e9ffa1c3f1b2e98134aa600c527692b8
DeviDivya <DeviDivya@bfusa.com>
2026-03-25 17:09:27 +0530

    created routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Login from "./components/Login";
+import Body from "./components/Body";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <>
-      <h1>Hello, React!</h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
